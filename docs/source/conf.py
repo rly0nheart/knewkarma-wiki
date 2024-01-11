83d45991bc130cbf9f0d90005924b21181bf4e45
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2024-01-11 07:34:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,16 @@
+from datetime import date
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Knew Karma Wiki'
+author = 'Richard Mwewa'
+copyright = f'{date.today().year}, {author}'
 
-release = '0.1'
-version = '0.1.0'
+
+release = '4.0'
+version = '4.0.0'
 
 # -- General configuration
 
